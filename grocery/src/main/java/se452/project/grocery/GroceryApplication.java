7f2ee522c475cd432dfd61abbebf92909f9b1f6e
Shupermokey <a.e.sobieraj@gmail.com>
2022-09-12 09:54:14 -0500

    First commit

---
+++ grocery/src/main/java/se452/project/grocery/GroceryApplication.java
@@ -0,0 +1,13 @@
+package se452.project.grocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryApplication.class, args);
+	}
+
+}
---
+++ grocery/src/test/java/se452/project/grocery/GroceryApplicationTests.java
@@ -0,0 +1,13 @@
+package se452.project.grocery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
