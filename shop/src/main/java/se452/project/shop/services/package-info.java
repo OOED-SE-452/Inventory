74f98a982753b71df76bfb80254dd6f4617adbcd
fengwang324 <111786529+fengwang324@users.noreply.github.com>
2022-11-08 08:03:59 -0600

    Update package-info.java

--- shop/src/main/java/se452/project/shop/services/package-info.java
+++ shop/src/main/java/se452/project/shop/services/package-info.java
@@ -1,6 +1,6 @@
 /**
  * @author Feng Wang
- * Account creation and login with security
+ * Account creation and login with security, and able to use Account in the grocery to login
  */
 
  package se452.project.shop.services;
