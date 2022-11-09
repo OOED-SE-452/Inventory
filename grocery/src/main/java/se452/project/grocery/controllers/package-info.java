233c97f6c86546184fd9d3a416a443de81a034f9
Shupermokey <a.e.sobieraj@gmail.com>
2022-11-09 11:19:21 -0600

    Package Info

--- grocery/src/main/java/se452/project/grocery/controllers/package-info.java
+++ grocery/src/main/java/se452/project/grocery/controllers/package-info.java
@@ -1,6 +1,21 @@
 /**
  * @author Feng Wang
  * Security check for get request, error page redirection, initial version of itemController
+ * 
+ * @author Alex Sobieraj
+ * 
+ * Created Account, AdminHome, and Login Controllers
+ * 
+ * Learned how to validate our Objects with the @Valid attribute, and how to check for
+ * the errors in a bindingresult
+ * 
+ * Learned you can attach a request mapping on top of the class
+ * - we originally were incorporating a User and an Admin Account type. We were using
+ * different Controller classes to handle /admin and /user. We scrapped this idea because the
+ * only functionality a User had was to search for items, which you can do with an account
+ * in the Shop app
+ * 
+ * 
  */
 
 package se452.project.grocery.controllers;
--- grocery/src/main/java/se452/project/grocery/services/package-info.java
+++ grocery/src/main/java/se452/project/grocery/services/package-info.java
@@ -4,6 +4,14 @@
  */
 /**
  * 
+ * @author Alex Sobieraj
+ * Created Services for Account, AccountMango, and ItemMango
+ *  Implemented the createAccount, loginAccount, getRole and getAccount methods for Account
+ *  and AccountMango
+ *  Implemented the findAll method and helped refactor the code in ItemService
+ *  Feng and I worked in parallel to create the ItemService and AccountServiceMango 
+ *  and updated the code to work with our new AccountMango and ItemMango objects/ MongoDb database
+ *  
  *
  */
 package se452.project.grocery.services;
